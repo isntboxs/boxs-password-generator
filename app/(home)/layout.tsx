e730443789c67ecd05e735ca9fbfe77efaf685f7
mrboxs <rizkywanda200155@gmail.com>
2024-09-29 22:57:12 +0700

    Refactor code structure and add global styles
    - Move global styles from "./globals.css" to "@/styles/globals.css"
    - Update font paths in "@/app/layout.tsx"
    - Add theme provider in "@/app/layout.tsx" to enable theme switching
    - Update metadata in "@/app/layout.tsx" to use siteConfig values

---
+++ app/(home)/layout.tsx
@@ -0,0 +1,8 @@
+function HomeLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      {children}
+    </div>
+  );
+}
+export default HomeLayout;
---
+++ config/site-config.ts
@@ -0,0 +1,14 @@
+export const siteConfig = {
+  title: "Password Generator | by @mrboxs",
+  description:
+    "Create a strong password with this tool. It's free and easy to use.",
+  url: "/",
+  author: "MrBoxs",
+  authorUrl: {
+    github: "https://github.com/mrboxs",
+    twitter: "https://twitter.com/isntboxs",
+    instagram: "https://instagram.com/mrboxs",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
